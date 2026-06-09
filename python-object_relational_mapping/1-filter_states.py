f44cda606a55daa2e91644ecec1ca11c9a12c009
22niko-spw <nicosojd@gmail.com>
2026-06-09 13:49:43 +0200

    Fix: pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -22,9 +22,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states"
+    cursor.execute("SELECT * FROM states "
                    "WHERE name LIKE BINARY 'N%' "
-                   "ORDER BY id""")
+                   "ORDER BY id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
