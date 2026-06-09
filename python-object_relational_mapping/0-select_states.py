ef98142cc690f8973991fdf8eb2545a374894d18
22niko-spw <nicosojd@gmail.com>
2026-06-09 11:13:59 +0200

    Fix:pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,30 +2,31 @@
 """
 Module to list state from a database
 """
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
 
-	user = sys.argv[1]
-	password = sys.Argv[2]
-	data_base = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.Argv[2]
+    data_base = sys.argv[3]
 
-	db = MySQLdb.connect(
-		host="localhost",
-		port=3306,
-		user=user,
-		passwd=password,
-		db=data_base
-	)
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username
+        passwd=password,
+        db=data_base
+    )
 
-	cursor = db.cursor()
-	
-	cursor.execute("SELECT * FROM states ORDER BY id")
-	rows = cursor.fetchall()
+    cursor = conn.cursor()
 
-	for row in rows:
-		print(row)
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    rows = cursor.fetchall()
 
-	cursor.close()
-	db.close()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
