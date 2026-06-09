691ad0f2cccc96e817b47851a9d6aec5f7dff89d
22niko-spw <nicosojd@gmail.com>
2026-06-09 14:04:51 +0200

    Add: a script that take 4arg

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module to list state from a database
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    data_base = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=data_base
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states "
+                   "WHERE name = '{}' "
+                   "ORDER BY id ASC".format(state_name_searched))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
