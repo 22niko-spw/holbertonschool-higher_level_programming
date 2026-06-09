c22c52fe603cd6d7c79bc9c775cedbee533c8553
22niko-spw <nicosojd@gmail.com>
2026-06-09 10:53:48 +0200

    Add: a script that list all states from db

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+import MySQLdb
+import sys
+
+conn = MySQLdb.connect(
+	host="localhost",
+	port=3306,
+	user=sys.argv[1],
+	passwd=sys.argv[2],
+	db=sys.argv[3])
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
